a0bbde3cad7310f635ec75e6e4865573f6c051d5
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2023-02-04 00:25:38 -0500

    made the drawing work

---
+++ README.md
@@ -0,0 +1,3 @@
+1. run "npm install" in ur terminal
+2. run "npm install -g http-server"
+3. start with command "http-server"
--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,36 @@
 window.addEventListener('load', () => {
+    console.log("page loaded");
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
 
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    ctx.fillRect(100,100,200,200);
+
+    let painting = false;
+
+    function startPos(){
+        painting=true;
+    }
+    function endPos(){
+        painting=false;
+    }
+
+    function draw(e){
+        console.log("draw");
+        if (!painting) return;
+        ctx.lineWidth = 10;
+        ctx.lineCap = "round";
+
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+    }
+
+    canvas.addEventListener("mousedown", startPos);
+    canvas.addEventListener("mouseup", endPos);
+    canvas.addEventListener("mousemove", draw);
+
 });
-
-let painting = false;
-
-function startPos(){
-    painting=true;
-}
-function endPos(){
-    painting=false;
-}
-
-function draw(elem){
-    if (!painting) return;
-    ctx.lineWidth = 10;
-    ctx.lineCap = "round";
-
-    ctx.lineTo(e.clientX, e.clientY);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(e.clientX, e.clientY);
-}
-
-canvas.addEventListener("mousedown", startPos);
-canvas.addEventListener("mouseup", endPos);
-canvas.addEventListener("mousemove", draw);
